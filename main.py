e6a9ddfd714ac113164d6a76a6201243893bf95c
sutiwat41 <napatee3004@gmail.com>
2020-01-02 16:45:11 +0700

    first draft

--- main.py
+++ main.py
@@ -17,11 +17,3 @@ keydf.grantData()
 
 
 #------- grant answer -------#
-"""
-ansdict = dict()
-ansexcel = pd.read_excel(ans_xlsx_file)
-ans_col = ansexcel.columns.values
-
-ind = list(ans_col).index('รหัสประจำตัวสอบ')
-ind_1 = list(ans_col).index('ข้อที่ 1')
-for i in ansexcel.get_values(): ansdict[i[ind]] = list(i[ind_1:ind_1+70]) """
